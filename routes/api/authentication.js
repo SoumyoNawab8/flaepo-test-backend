da7dc39a02e31fed4b4b000944b37b07f357a6fd
SoumyoNawab8 <gtechxd@gmail.com>
2019-04-04 10:55:16 +0530

    added templates with handlebars

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -4,17 +4,33 @@ const db=require('../../config/dBase');
 const User=require('../../models/User');
 const jwt = require('jsonwebtoken');
 
+function ValidateFields(fields){
+  let missingfields=[];
+  Object.keys(fields).map(key=>{
+    if(fields[key].length==0){
+      missingfields.push(key+" can't be empty!");
+    }
+  })
+
+  if(missingfields.length>0){
+    return missingfields;
+  }
+  else{
+    return true;
+  }
+}
 
 
 
 router.post('/login',(req,res,next)=>{
+  console.log(req.body)
   if(req.body.email==undefined && req.body.password==undefined){
     res.send('Email and password is required!')
   }
 
   if(req.body.email.length!=0 && req.body.password.length!=0){
-    User.findOne({where:req.body}).then(result=>{
-      if(result.id!=undefined){
+    User.findOne({where:{email:req.body.email,password:req.body.password}}).then(result=>{
+      if(result!=null){
             jwt.sign({id:result.id,name:result.name}, 'falepo_secret', {
       expiresIn: 3600
     }, (err, token) => {
@@ -22,8 +38,12 @@ router.post('/login',(req,res,next)=>{
         Success: true,
         Token: 'Bearer ' + token
       });});
+
       }
-    })
+      else{
+        console.log(result);
+      }
+    }).catch(err=>console.log(err))
 
   }
   else{
@@ -35,11 +55,23 @@ router.post('/login',(req,res,next)=>{
 // router.get('/users')
 
 router.post('/register',function(req,res){
+  let passFields=ValidateFields(req.body);
+  if(passFields==true){
     User.create(req.body)
     .then((result=>{
-        res.send(result)
+        // res.send(result)
+        res.redirect('/login');
     }))
     .catch(err=>{res.send(err)})
+  }
+  else{
+    let errString="";
+    passFields.map(item=>{
+      passFields.length==0?errString=item:errString+="<br/> "+item;
+    })
+    res.send(errString)
+  }
+    
 })
 
 module.exports=router;
--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express=require('express');
 const app=express();
 const db=require('./config/dBase');
 const bodyParser=require('body-parser');
+const path=require('path');
 const passport = require('passport');
-
+const exphbs=require('express-handlebars');
 
 
 db.authenticate()
 .then(()=>console.log('Databse Connected'))
 .catch((err)=>console.log('Error: '+err));
+
+app.set('views',path.join(__dirname,'views'));
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(passport.initialize());
+
+app.use(express.static(path.join(__dirname,'public')));
 
 require ('./config/passport')(passport)
 app.get('/error',(req,res)=>{
@@ -19,6 +27,21 @@ app.get('/error',(req,res)=>{
 })
 // app.use(passport.session());
 
+app.get('/',(req,res)=>{
+    res.render('home',{
+        imgUrl:'/images/bg2.jpg',
+        title:"We are rendering from HandleMars"})
+})
+
+app.get('/register',(req,res)=>{
+    res.render('register',{
+        title:"Registration | LUCID DREAMS"});
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login',{
+        title:"Login | LUCID DREAMS"});
+})
 
 
 app.use('/users',require('./routes/api/users'));
